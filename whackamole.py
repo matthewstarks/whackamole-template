2de2fe542acb3ae312fd2c30ea64183700a63778
Matthew Starks <ukandit@hotmail.com>
2024-11-13 17:20:27 -0500

    Added functionality to the game

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -9,12 +9,37 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        color = "black"
+
+        screen.fill("light green")
+        for i in range(1, 16):
+            pygame.draw.line(screen, color, (0, i * 32), (640, i * 32))
+        for i in range(1, 20):
+            pygame.draw.line(screen, color, (i * 32, 0), (i * 32, 640))
+        screen.blit(mole_image, mole_image.get_rect(topleft = (0, 0)))
+        molex = 0
+        moley = 0
+
+
         running = True
         while running:
             for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    row = x // 32
+                    col = y // 32
+                    if row == molex and col == moley:
+                        molex = random.randint(0, 19)
+                        moley = random.randint(0, 15)
+                        screen.fill("light green")
+                        for i in range(1, 16):
+                            pygame.draw.line(screen, color, (0, i * 32), (640, i * 32))
+                        for i in range(1, 20):
+                            pygame.draw.line(screen, color, (i * 32, 0), (i * 32, 640))
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(molex * 32, moley * 32)))
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+
             pygame.display.flip()
             clock.tick(60)
     finally:
